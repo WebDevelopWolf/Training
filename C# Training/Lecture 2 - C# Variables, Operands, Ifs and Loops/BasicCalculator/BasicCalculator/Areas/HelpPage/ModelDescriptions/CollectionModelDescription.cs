fb0c1d2dafcf60dafe7373d3fdb8d4d3129b1bf5
Liane Stevenson <knightowlcrm@gmail.com>
2018-03-07 23:03:01 +0000

    CodeUpSOT - Lecture 2
    Lecture 2 slides and resources

--- C# Training/Lecture 2 - C# Variables, Operands, Ifs and Loops/BasicCalculator/BasicCalculator/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace BasicCalculator.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- C# Training/Lecture 2 - C# Variables, Operands, Ifs and Loops/BasicCalculator/BasicCalculator/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BasicCalculator.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
